44386f1ce7289c1920d296e822edbc42a748c6ea
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2025-11-28 13:35:03 +0100

    The Pusblisher NewAgency will be able to add , remove and notify

--- NewsAgency.cs
+++ NewsAgency.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ObserverPattern
 {
-    internal class NewsAgency
+    internal class NewsAgency : ISubject
     {
+        private List<IObserver> _observers = [];
+        public void RegisterObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+        public void RemoveObserver(IObserver observer)
+        { 
+            _observers.Remove(observer);
+        }
+        public void NotifyObservers(string news)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(news);
+            }
+        }
     }
 }
