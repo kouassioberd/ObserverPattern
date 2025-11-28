c6b4fdf0e0c9026a4f99037f522aa27056b2e425
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2025-11-28 13:33:17 +0100

    User class created and identified by name

--- User.cs
+++ User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ObserverPattern
 {
-    internal class User
+    internal class User(string name) : IObserver
     {
+        private string _name = name;
+        public void Update(string news)
+        {
+            Console.WriteLine($"User {_name} received news:   { news}");
+        }
     }
 }
